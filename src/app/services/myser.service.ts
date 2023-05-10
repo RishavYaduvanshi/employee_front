1c0faec07aac50b18b3ea67b4f7b6e82e82983a9
RishavYaduvanshi <rishavkhiladi768@gmail.com>
2023-05-10 19:10:55 +0530

    connection_ull

--- src/app/services/myser.service.ts
+++ src/app/services/myser.service.ts
@@ -7,32 +7,34 @@ import { HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class MyserService {
-
-  constructor(private http : HttpClient) { }
+  URL_BASE : string
+  constructor(private http : HttpClient) { 
+    this.URL_BASE = "https://rishav.d3m0n1k.engineer"
+  }
   url = 'https://mocki.io/v1/5ec525a6-5606-4d79-b405-61c06401fe55'
-  url1 = 'https://localhost:7052/Department/GetAllDepartments'
-  url2 = 'https://localhost:7052/Employee/GetAllEmployee'
-  url3 = 'https://localhost:7052/Employee/AddEmployee'
-  url4 = 'https://localhost:7052/Employee/DeleteEmployee/'
-  url5 = 'https://localhost:7052/Employee/UpdateEmployee/'
-  url6 = 'https://localhost:7052/Department/AddDepartment'
-  url7 = 'https://localhost:7052/Department/DeleteDepartment/'
-  url8 = 'https://localhost:7052/Department/UpdateDepartment/'
-  url9 = 'https://localhost:7052/Project/GetAllProjects'
-  url10 = 'https://localhost:7052/Project/AddProject'
-  url11 = 'https://localhost:7052/Project/UpdateProject/'
-  url12 = 'https://localhost:7052/Project/DeleteProject/'
-  url13 = 'https://localhost:7052/SignUpRole/SignIn'
-  url14 = 'https://localhost:7052/SignUpRole/AddSignUp'
+  url1 = '/Department/GetAllDepartments'
+  url2 = '/Employee/GetAllEmployee'
+  url3 = '/Employee/AddEmployee'
+  url4 = '/Employee/DeleteEmployee/'
+  url5 = '/Employee/UpdateEmployee/'
+  url6 = '/Department/AddDepartment'
+  url7 = '/Department/DeleteDepartment/'
+  url8 = '/Department/UpdateDepartment/'
+  url9 = '/Project/GetAllProjects'
+  url10 = '/Project/AddProject'
+  url11 = '/Project/UpdateProject/'
+  url12 = '/Project/DeleteProject/'
+  url13 = '/SignUpRole/SignIn'
+  url14 = '/SignUpRole/AddSignUp'
 
   //forgot
 
-  url15 = 'https://localhost:7052/SignUpRole/ForgotPassword'
-  url16 = 'https://localhost:7052/SignUpRole/ResetPassword'
+  url15 = '/SignUpRole/ForgotPassword'
+  url16 = '/SignUpRole/ResetPassword'
 
   // emailverfied
 
-  url17 = 'https://localhost:7052/SignUpRole/EmailVerfiedDone'
+  url17 = '/SignUpRole/EmailVerfiedDone'
 
 
   
@@ -58,62 +60,62 @@ export class MyserService {
   }
   
   getDepartment(){
-    return this.http.get(this.url1,{headers:this.getHeaders()});
+    return this.http.get(this.URL_BASE+this.url1,{headers:this.getHeaders()});
   }
   getUser(){
-    return this.http.get(this.url2, {headers:this.getHeaders()});
+    return this.http.get(this.URL_BASE+this.url2, {headers:this.getHeaders()});
   }
   addUser(data : any){
-    return this.http.post(this.url3,data, {headers:this.getHeaders()})
+    return this.http.post(this.URL_BASE+this.url3,data, {headers:this.getHeaders()})
   }
   deleteUser(id:number){
-    return this.http.delete(this.url4+id,{headers:this.getHeaders()});
+    return this.http.delete(this.URL_BASE+this.url4+id,{headers:this.getHeaders()});
   }
   updateUser(id : number, data : any){
-    return this.http.put(this.url5+id,data,{headers:this.getHeaders()});
+    return this.http.put(this.URL_BASE+this.url5+id,data,{headers:this.getHeaders()});
   }
 
   addDep(data:any){
-    return this.http.post(this.url6,data,{headers:this.getHeaders()})
+    return this.http.post(this.URL_BASE+this.url6,data,{headers:this.getHeaders()})
   }
   deleteDep(id:number){
-    return this.http.delete(this.url7+id,{headers:this.getHeaders()});
+    return this.http.delete(this.URL_BASE+this.url7+id,{headers:this.getHeaders()});
   }
   updateDep(id : number, data : any){
-    return this.http.put(this.url8+id,data,{headers:this.getHeaders()});
+    return this.http.put(this.URL_BASE+this.url8+id,data,{headers:this.getHeaders()});
   }
 
   getProjList(){
-    return this.http.get(this.url9,{headers:this.getHeaders()});
+    return this.http.get(this.URL_BASE+this.url9,{headers:this.getHeaders()});
   }
 
   addProj(data:any){
-    return this.http.post(this.url10,data,{headers:this.getHeaders()})
+    return this.http.post(this.URL_BASE+this.url10,data,{headers:this.getHeaders()})
   }
   updateProj(id : number, data : any){
-    return this.http.put(this.url11+id,data,{headers:this.getHeaders()});
+    return this.http.put(this.URL_BASE+this.url11+id,data,{headers:this.getHeaders()});
   }
   deleteProj(id:number){
-    return this.http.delete(this.url12+id,{headers:this.getHeaders()});
+    return this.http.delete(this.URL_BASE+this.url12+id,{headers:this.getHeaders()});
   }
 
   login(data:any){
-    return this.http.post(this.url13,data)
+    return this.http.post(this.URL_BASE+this.url13,data)
   }
 
   signup(data:any){
-    return this.http.post(this.url14,data)
+    return this.http.post(this.URL_BASE+this.url14,data)
   }
   checkEmail(email: string){
     const params = { email };
-    return this.http.get(this.url15, { params });
+    return this.http.get(this.URL_BASE+this.url15, { params });
   }
 
 
   resetPassword(Email:string, NewPassword:string){
-    return this.http.get(this.url16,{params: {Email,NewPassword}});
+    return this.http.get(this.URL_BASE+this.url16,{params: {Email,NewPassword}});
   }
   emailVerfiedDone(Email:string){
-    return this.http.get(this.url17,{params: {Email}});
+    return this.http.get(this.URL_BASE+this.url17,{params: {Email}});
   }
 }
